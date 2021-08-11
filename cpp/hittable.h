17dbc1983c67ea0f67fb44c1bdb4459f84bf4bd4
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-11 13:13:18 +0900

    hittableクラスを書いた

---
+++ cpp/hittable.h
@@ -0,0 +1,23 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+struct hit_record
+{
+	point3 p;
+	vec3 normal;
+	double t;
+};
+
+class hittable
+{
+private:
+	/* data */
+public:
+	virtual ~hittable();
+
+	virtual bool hit(const ray&r,double t_min,double t_max,hit_record&rec)const=0;
+};
+
+#endif
