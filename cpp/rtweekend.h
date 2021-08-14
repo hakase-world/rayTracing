b88b5a486f006a8fa9c226eac33709c1fc1d8a3a
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-14 17:34:29 +0900

    [0.0, 1.0]の範囲のランダムな倍精度浮動小数を返す関数を書いた

--- cpp/rtweekend.h
+++ cpp/rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 //定数
 const double infinity = std::numeric_limits<double>::infinity();
@@ -13,6 +14,13 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radiaans(double degrees)
 {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937_64 generator;
+	return distribution(generator);
 }
 
 //共通ヘッダー
