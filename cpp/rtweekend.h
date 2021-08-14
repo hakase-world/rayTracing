76ddfcef001cdc61f5cc99bd5e64fe577a63b5d4
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-15 04:56:19 +0900

    [min,max]の範囲のランダムな倍精度小数を返す関数を書いた

--- cpp/rtweekend.h
+++ cpp/rtweekend.h
@@ -23,9 +23,18 @@ inline double random_double()
 	return distribution(generator);
 }
 
-inline double clamp(double x,double min,double max){
-	if(x < min) return min;
-	if(x > max) return max;
+inline double random_double(double min, double max)
+{
+	// Returns a randodm real in [min,max].
+	return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+	if (x < min)
+		return min;
+	if (x > max)
+		return max;
 	return x;
 }
 
