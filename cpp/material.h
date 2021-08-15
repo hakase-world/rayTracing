d02a07c6c61d07a73bf3c20a563f42c8cbfdf5d5
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-16 00:31:32 +0900

    lambertianクラスを書いた

--- cpp/material.h
+++ cpp/material.h
@@ -12,4 +12,21 @@ public:
 	virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const = 0;
 };
 
+class lambertian : public material
+{
+public:
+	lambertian(const color &a) : albedo(a) {}
+
+	virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override
+	{
+		auto scatter_direction = rec.normal + vec3::random_unit_vector();
+		scattered = ray(rec.p, scatter_direction);
+		attenuation = albedo;
+		return true;
+	}
+
+public:
+	color albedo;
+};
+
 #endif
