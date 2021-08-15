fb77a6a029639d2fbd34c43f708d5a427695d1fc
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-16 02:05:54 +0900

    worldに金属球を追加した

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,6 +4,7 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include "material.h"
 
 #include <iostream>
 
@@ -55,8 +56,11 @@ int main()
 
 	// World
 	hittable_list world;
-	world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+	world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5, std::make_shared<lambertian>(color(0.7, 0.3, 0.3))));
+	world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100, std::make_shared<lambertian>(color(0.8, 0.8, 0.0))));
+	world.add(std::make_shared<sphere>(point3(1, 0, -1), 0.5, std::make_shared<metal>(color(.8, .6, .2))));
+	world.add(std::make_shared<sphere>(point3(-1, 0, -1), 0.5, std::make_shared<metal>(color(.8, .8, .8))));
 
 	// Camera
 	camera cam;
