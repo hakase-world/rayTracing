78d63b0375a69b3da4e83f81bbfce27d3b6cc365
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-16 01:54:25 +0900

    metalクラスを書いた

--- cpp/material.h
+++ cpp/material.h
@@ -29,4 +29,22 @@ public:
 	color albedo;
 };
 
+class metal : public material
+{
+public:
+	metal(const color &a) : albedo(a) {}
+
+	virtual bool scatter(
+		const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const
+	{
+		vec3 reflected = vec3::reflect(vec3::unit_vector(r_in.direction()), rec.normal);
+		scattered = ray(rec.p, reflected);
+		attenuation = albedo;
+		return (vec3::dot(scattered.direction(), rec.normal) > 0);
+	}
+
+public:
+	color albedo;
+};
+
 #endif
