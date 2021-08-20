6430cf77cf1c5962305cb45740d0e8ee72a08fac
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-20 17:59:08 +0900

    get_ray関数の返り値を修正した

--- cpp/camera.h
+++ cpp/camera.h
@@ -39,7 +39,7 @@ ray camera::get_ray(double s, double t) const
 {
 	vec3 rd = lens_radius * random_in_unit_disk();
 	vec3 offset = u * rd.x() + v * rd.y();
-	return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin);
+	return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin-offset);
 }
 
 #endif
